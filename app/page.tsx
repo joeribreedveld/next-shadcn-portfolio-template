0a5b9c1ae74f69375ad833c471025e658a69bf81
joeribreedveld <joeri.breedveld123@gmail.com>
2024-05-19 17:41:18 +0200

    feat: make header responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Github } from "lucide-react";
+
 export default function Page() {
-  return <div>Hello, World!</div>;
+  return (
+    <main className="container py-32 flex items-center flex-col">
+      <Badge variant="secondary">Website in maintenance</Badge>
+      <h1 className="scroll-m-20 text-4xl mt-8 font-extrabold tracking-tight lg:text-5xl max-w-2xl text-center">
+        Front-end developer driven by learning and challenges.
+      </h1>
+      <p className="text-lg font-light max-w-xl text-center text-foreground mt-6">
+        A modal dialog that interrupts the user with important content and
+        expects a response.
+      </p>
+      <div className="mt-8 flex gap-4 items-center">
+        <Button>Learn more</Button>
+        <Button variant="outline">
+          <Github className="mr-2 h-4 w-4" />
+          GitHub
+        </Button>
+      </div>
+    </main>
+  );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -9,44 +9,69 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { MenuIcon } from "lucide-react";
 
 export default function Header() {
   return (
     <header className="flex items-center py-4 container">
-      <div className="w-full flex justify-start items-center">
+      <Sheet>
+        <SheetTrigger asChild className="w-ful">
+          <div className="w-full md:hidden">
+            <Button variant="outline" size="icon">
+              <MenuIcon className="h-6 w-6" />
+            </Button>
+          </div>
+        </SheetTrigger>
+
+        <SheetContent side="left">
+          <Link className="flex items-center gap-2" href="#">
+            <h3 className="text-xl font-semibold tracking-tight">
+              Joeri Breedveld
+            </h3>
+          </Link>
+          <ul className="flex flex-col gap-3 mt-4">
+            <li>
+              <Link href="#">Home</Link>
+            </li>
+            <li>
+              <Link href="#">Project</Link>
+            </li>
+            <li>
+              <Link href="#">About</Link>
+            </li>
+            <li>
+              <Link href="#">Experience</Link>
+            </li>
+          </ul>
+        </SheetContent>
+      </Sheet>
+      <div className="w-full md:flex hidden justify-start items-center">
         <h3 className="text-xl font-semibold tracking-tight">
           Joeri Breedveld
         </h3>
       </div>
-      <NavigationMenu className="w-full flex items-center justify-center">
+      <NavigationMenu className="w-full items-center justify-center hidden md:flex">
         <NavigationMenuList>
           <NavigationMenuItem>
-            <Link href="#">
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-              </NavigationMenuLink>
-            </Link>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Home
+            </NavigationMenuLink>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="#">
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Projects
-              </NavigationMenuLink>
-            </Link>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Projects
+            </NavigationMenuLink>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="#">
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                About
-              </NavigationMenuLink>
-            </Link>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              About
+            </NavigationMenuLink>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="#">
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Experience
-              </NavigationMenuLink>
-            </Link>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Experience
+            </NavigationMenuLink>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
