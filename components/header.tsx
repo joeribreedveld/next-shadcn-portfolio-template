7782505d12b2767bb736d7128fbdac2aa7d0f69a
joeribreedveld <joeri.breedveld123@gmail.com>
2024-05-19 19:47:29 +0200

    feat: header update and projects content update

--- components/header.tsx
+++ components/header.tsx
@@ -15,39 +15,8 @@ import { MenuIcon } from "lucide-react";
 export default function Header() {
   return (
     <header className="flex items-center py-4 px-4 md:container">
-      <Sheet>
-        <SheetTrigger asChild className="w-ful">
-          <div className="w-full md:hidden">
-            <Button variant="outline" size="icon">
-              <MenuIcon className="h-6 w-6" />
-            </Button>
-          </div>
-        </SheetTrigger>
-
-        <SheetContent side="left">
-          <Link className="flex items-center gap-2" href="#">
-            <h3 className="text-lg font-semibold tracking-tight">
-              Joeri Breedveld
-            </h3>
-          </Link>
-          <ul className="flex flex-col gap-3 mt-4">
-            <li>
-              <Link href="#">Home</Link>
-            </li>
-            <li>
-              <Link href="#">Project</Link>
-            </li>
-            <li>
-              <Link href="#">About</Link>
-            </li>
-            <li>
-              <Link href="#">Experience</Link>
-            </li>
-          </ul>
-        </SheetContent>
-      </Sheet>
-      <div className="w-full md:flex hidden justify-start items-center">
-        <h3 className="text-lg font-semibold tracking-tight">
+      <div className="w-full justify-start items-center">
+        <h3 className="text-xl md:text-lg font-semibold tracking-tight">
           Joeri Breedveld
         </h3>
       </div>
@@ -70,9 +39,40 @@ export default function Header() {
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
-      <div className="w-full flex justify-end items-center">
+      <div className="w-full justify-end items-center hidden md:flex">
         <Button>Contact</Button>
       </div>
+      <Sheet>
+        <SheetTrigger asChild className="w-full flex justify-end items-center">
+          <div className="w-full md:hidden">
+            <Button variant="outline" size="icon">
+              <MenuIcon className="h-6 w-6" />
+            </Button>
+          </div>
+        </SheetTrigger>
+
+        <SheetContent>
+          <Link className="flex items-center gap-2" href="#">
+            <h3 className="text-xl md:text-lg font-semibold tracking-tight">
+              Joeri Breedveld
+            </h3>
+          </Link>
+          <ul className="flex flex-col gap-3 mt-4">
+            <li>
+              <Link href="#">Home</Link>
+            </li>
+            <li>
+              <Link href="#">Project</Link>
+            </li>
+            <li>
+              <Link href="#">About</Link>
+            </li>
+            <li>
+              <Link href="#">Experience</Link>
+            </li>
+          </ul>
+        </SheetContent>
+      </Sheet>
     </header>
   );
 }
--- components/projects.tsx
+++ components/projects.tsx
@@ -6,24 +6,64 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Image from "next/image";
+import { Button } from "./ui/button";
+import { Github, Link } from "lucide-react";
+
+const projects = [
+  {
+    title: "Appie Family",
+    description:
+      "Shared grocery list app for households. Let your family add items to the list and see them in real-time.",
+    image:
+      "https://source.unsplash.com/a-mother-unpacking-local-food-in-zero-waste-packaging-from-bag-with-help-of-daughter-in-kitchen-at-home-mG9ACs8XFTE",
+  },
+  {
+    title: "Martial Arts Amsterdam",
+    description:
+      "An online platform for discovering and promoting martial arts dojos and workshops in Amsterdam.",
+    image:
+      "https://source.unsplash.com/boy-doing-karate-routines-during-golden-hour-igLzPKOvZNw",
+  },
+  {
+    title: "Hair Calendar",
+    description:
+      "Track your hair wash days and adjust your hair routine based on a personalized calendar.",
+    image:
+      "https://source.unsplash.com/a-woman-getting-her-hair-cut-by-a-hair-stylist-Md_DhaFsnCQ",
+  },
+];
 
 export default function Projects() {
   return (
     <section className="mx-auto py-8 md:py-12 md:pb-8 lg:py-16 lg:pb-20 md:grid-cols-2 grid lg:grid-cols-3 w-full gap-4">
-      {[...Array(6)].map((index) => (
-        <Card key={index} className="w-full">
+      {projects.map((project, index) => (
+        <Card key={index} className="w-full flex flex-col justify-between">
           <CardHeader>
-            <CardTitle>Project title</CardTitle>
-            <CardDescription>Project description</CardDescription>
+            <CardTitle className="text-xl">{project.title}</CardTitle>
+            <CardDescription>{project.description}</CardDescription>
           </CardHeader>
           <CardContent>
             <Image
-              src="/images/placeholder.jpg"
+              src={project.image}
               alt="Project"
-              width={150}
-              height={150}
-              className="rounded-lg w-full"
+              width={128}
+              height={128}
+              className="rounded-lg w-full aspect-[3/2] object-cover"
             />
+            <ul className="flex gap-4">
+              <li>
+                <Button variant="outline" className="mt-6">
+                  <Github className="h-4 w-4 mr-2" />
+                  GitHub
+                </Button>
+              </li>
+              <li>
+                <Button variant="outline" className="mt-6">
+                  <Link className="h-4 w-4 mr-2" />
+                  Website
+                </Button>
+              </li>
+            </ul>
           </CardContent>
         </Card>
       ))}
